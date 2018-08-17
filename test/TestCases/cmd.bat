83dd572398cf23813aead75c5f32c34aaf892fd9
jinjun.ljj <jinjun.ljj@alibaba-inc.com>
2018-08-17 09:29:30 +0800

    COMPILE_BUG

--- test/TestCases/cmd.bat
+++ test/TestCases/cmd.bat
@@ -1,4 +1,4 @@
 set dir=../../CSharp.lua.Launcher/bin/Debug/netcoreapp2.0/
-dotnet "%dir%CSharp.lua.Launcher.dll" -s src -d out -a "TestCase"
+dotnet "%dir%CSharp.lua.Launcher.dll" -s src -d out -a "TestCase" -csc"-define:COMPILE_BUG"
 "../__bin/lua5.1/lua" launcher.lua
 pause
