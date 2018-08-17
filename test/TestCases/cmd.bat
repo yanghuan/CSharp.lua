6fc69d4a6774a94cf55523f0b5755d29f2772b13
jinjun.ljj <jinjun.ljj@alibaba-inc.com>
2018-08-17 09:17:40 +0800

    TestCase Attribute

--- test/TestCases/cmd.bat
+++ test/TestCases/cmd.bat
@@ -1,3 +1,3 @@
 set dir=../../CSharp.lua.Launcher/bin/Debug/netcoreapp2.0/
-dotnet "%dir%CSharp.lua.Launcher.dll" -s src -d out
+dotnet "%dir%CSharp.lua.Launcher.dll" -s src -d out -a "TestCase"
 "../__bin/lua5.1/lua" launcher.lua
