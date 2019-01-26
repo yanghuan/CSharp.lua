fbd172d33e8154060f97c6dca6a03c0c946a9fcd
李锦俊 <jinjun.ljj@alibaba-inc.com>
2019-01-26 08:38:43 +0800

    上传单元测试

--- test/TestCases/src/Tests/LuaArrayTest.cs
+++ test/TestCases/src/Tests/LuaArrayTest.cs
@@ -11,6 +11,19 @@ namespace TestCases {
       var indexValue = arr[123];
       arr[456] = indexValue;
     }
+    public static void LuaObjectTest01() {
+      var obj = new LuaObject();
+      LuaObject obj0 = new LuaObject();
+      LuaObject obj1 = LuaArrayExternions.CreateLuaObject();
+      LuaObject obj2 = LuaArrayExternions.CreateLuaObjectWithMetaXml();
+    }
+
+    public static void LuaObjectTest02() {
+      // 不带MetaXml调用隐式转换函数
+      int obj2 = LuaArrayExternions.CreateLuaObject();
+      // 带MetaXml的调用隐式转换函数
+      int obj3 = LuaArrayExternions.CreateLuaObjectWithMetaXml();
+    }
 
   }
 }
