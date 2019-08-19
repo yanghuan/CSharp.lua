ee02183970c811d0fe027c87a5ba8ed9b2a38873
yanghuan <sy.yanghuan@gmail.com>
2019-08-19 18:00:53 +0800

    update action

--- test/BridgeNetTests/Tests/run.bat
+++ test/BridgeNetTests/Tests/run.bat
@@ -2,11 +2,3 @@ rem set version=LuaJIT-2.1.0-beta3
 set version=Lua5.3
 set lua=../../__bin/%version%/lua
 "%lua%" run.lua
-
-if not %errorlevel%==0 (
-  echo please see log, has some error.
-  goto :Fail 
-)
-
-:Fail
-rem pause
