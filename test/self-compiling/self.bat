cfd979f6d97657d69256161180beac649f541a60
yanghuan <sy.yanghuan@gmail.com>
2019-08-19 14:09:20 +0800

    remove dll dependence, make github action success

--- test/self-compiling/self.bat
+++ test/self-compiling/self.bat
@@ -1,5 +1,7 @@
+dotnet publish ../../ --configuration Release
+
+set lib=../../CSharp.lua.Launcher/bin/Release/netcoreapp2.0/publish/
 set dir=../../CSharp.lua.Launcher/bin/Debug/netcoreapp2.0/
-set lib=lib/
 set obj="../../CSharp.lua/obj"
 if exist %obj% rd /s /q %obj%
 dotnet "%dir%CSharp.lua.Launcher.dll" -s ../../CSharp.lua/ -d selfout -l %lib%Microsoft.CodeAnalysis.CSharp.dll;%lib%Microsoft.CodeAnalysis.dll -metadata
