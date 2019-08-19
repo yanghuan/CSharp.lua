7ed34627be22295f4e53c5f92c7117cd0cd17341
yanghuan <sy.yanghuan@gmail.com>
2019-08-19 15:50:12 +0800

    update action

---
+++ test/test.bat
@@ -0,0 +1,4 @@
+dotnet publish ../ --configuration Release --output bin/Release/PublishOutput
+set dir=../CSharp.lua.Launcher/bin/Release/PublishOutput/
+
+dotnet "%dir%CSharp.lua.Launcher.dll" -s fibonacci/src -d fibonacci/out
