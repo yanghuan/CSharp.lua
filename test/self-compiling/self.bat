7a04b3b618537713519f93f9e93f203c3290e2db
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-12 23:40:39 +0200

    Add Cake package .dll files to self-compile script.

--- test/self-compiling/self.bat
+++ test/self-compiling/self.bat
@@ -2,6 +2,6 @@ set dir=bin/Release/PublishOutput/
 dotnet publish ../../ --configuration Release --output %dir%
 set obj="../../CSharp.lua/obj"
 if exist %obj% rd /s /q %obj%
-dotnet "%dir%CSharp.lua.Launcher.dll" -s ../../CSharp.lua/ -d selfout -l %dir%Microsoft.CodeAnalysis.CSharp.dll;%dir%Microsoft.CodeAnalysis.dll -metadata
+dotnet "%dir%CSharp.lua.Launcher.dll" -s ../../CSharp.lua/ -d selfout -l %dir%Microsoft.CodeAnalysis.CSharp.dll;%dir%Microsoft.CodeAnalysis.dll;%dir%Cake.Common.dll;%dir%Cake.Core.dll;%dir%Cake.Incubator.dll -metadata
 rd /s /q bin
 pause
