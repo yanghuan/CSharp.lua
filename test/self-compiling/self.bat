923a8b19aa0b5c6e9ae1fbc66d69d0c4d0f5038c
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-13 20:42:52 +0200

    Add package .dlls to self-compilation script.

--- test/self-compiling/self.bat
+++ test/self-compiling/self.bat
@@ -2,6 +2,6 @@ set dir=bin/Release/PublishOutput/
 dotnet publish ../../ --configuration Release --output %dir%
 set obj="../../CSharp.lua/obj"
 if exist %obj% rd /s /q %obj%
-dotnet "%dir%CSharp.lua.Launcher.dll" -s ../../CSharp.lua/ -d selfout -l %dir%Microsoft.CodeAnalysis.CSharp.dll;%dir%Microsoft.CodeAnalysis.dll;%dir%Cake.Common.dll;%dir%Cake.Core.dll;%dir%Cake.Incubator.dll -metadata
+dotnet "%dir%CSharp.lua.Launcher.dll" -s ../../CSharp.lua/ -d selfout -l %dir%Microsoft.CodeAnalysis.CSharp.dll;%dir%Microsoft.CodeAnalysis.dll;%dir%Cake.Common.dll;%dir%Cake.Core.dll;%dir%Cake.Incubator.dll;%dir%NuGet.Common.dll;%dir%NuGet.Configuration.dll;%dir%NuGet.Frameworks.dll;%dir%NuGet.Packaging.dll;%dir%NuGet.Versioning.dll;%dir%Newtonsoft.Json.dll -metadata
 rd /s /q bin
 pause
