401abcc78ff99b88a0b290d7b5d84a0beaa57a9f
YANG Huan <sy.yanghuan@gmail.com>
2022-03-02 09:58:36 +0800

    fix  #389

--- convert.sh
+++ convert.sh
@@ -8,7 +8,7 @@ if [ -z "$srcp" ]; then
 fi
 
 if ! [ -d "$dstp" ]; then mkdir "$dstp"; fi
-dotnet ./CSharp.lua.Launcher/bin/Release/net5.0/CSharp.lua.Launcher.dll -s "$srcp" -d "$dstp/."
+dotnet ./CSharp.lua.Launcher/bin/Release/net6.0/CSharp.lua.Launcher.dll -s "$srcp" -d "$dstp/."
 converterr=$?
 if [ $converterr != 0 ]; then
         echo "errors occurred during convert">&2
